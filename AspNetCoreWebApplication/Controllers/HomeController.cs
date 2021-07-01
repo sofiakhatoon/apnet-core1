9ca8f01069d928cf6900ed9728a276158876c687
sofiakhatoon <44379240+sofiakhatoon@users.noreply.github.com>
2021-07-01 14:15:34 -0500

    Update HomeController.cs

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -12,8 +12,17 @@ namespace AspNetCoreWebApplication.Controllers
         public IActionResult Index()
         {
             ViewData["Message"] = "You just created a ASP.Net Core web application!";
-            string custReviewRole = null;
-            if(custReviewRole == null) return null;
+             Type t =typeof(int);
+      Object obj1 = typeof(int).GetTypeInfo();
+     
+ 
+
+      Object obj2 = typeof(String);
+     
+           if(t.Equals(obj2)||false){
+return null;
+      }
+           
             return View();
         }
 
