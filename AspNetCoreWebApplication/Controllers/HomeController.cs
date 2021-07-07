b7d4df546214c57e8b39a49dee42b476a52730b0
sofiakhatoon <44379240+sofiakhatoon@users.noreply.github.com>
2021-07-07 11:10:24 -0500

    Update HomeController.cs

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -7,8 +7,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreWebApplication.Controllers
 {
+
+public struct YodaString{
+    private string _value ;
+    public SodaString( string value ) 
+		{
+            this._value = value;
+		}
+}
+public abstract class CompositionPlugin { 
+    
+}
+public class MyCompositionPlugin : CompositionPlugin { 
+      private string _value ;
+    
+    public YodaString value {get{return new YodaString("F");} }
+    	public override bool Equals(object obj)
+		{
+			if ( ( _value == null ) || ( obj == null ) )
+				return false ;
+
+			
+
+			if (obj is string)
+				return this._value.Equals((string)obj);
+
+			throw new Exception("Cannot compare object of type [" + obj.GetType().FullName + "] to yodaString");
+		}
+}
     public class HomeController : Controller
     {
+     private static List<MyCompositionPlugin> GetList(int key){
+        List<MyCompositionPlugin> obj1 = new List<MyCompositionPlugin>();
+
+        obj1.Add(new MyCompositionPlugin());   
+        obj1.Add(new MyCompositionPlugin());   
+        obj1.Add(new MyCompositionPlugin());  
+        return obj1;
+    }
         public IActionResult Index()
         {
             ViewData["Message"] = "You just created a ASP.Net Core web application!";
